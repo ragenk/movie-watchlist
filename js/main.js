409907d7ab16be539aec3c19c751079bc6e01a04
Rafael Aguasvivas <100101905+ragenk@users.noreply.github.com>
2025-05-25 15:39:41 -0400

    Last commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const searchInput = document.getElementById("search");
 const searchBtn = document.getElementById("search-btn");
 const mainEl = document.getElementById("main-container");
-const apiKey = process.env.MOVIE_API_KEY;
 let watchlist = JSON.parse(localStorage.getItem("moviesWatchlist")) || [];
 let moviesByIds;
 
@@ -17,7 +16,7 @@ searchBtn.addEventListener("click", performSearch);
 async function performSearch() {
   try {
     const response = await fetch(
-      `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput.value}`
+      `https://www.omdbapi.com/?apikey=f3f350ca&s=${searchInput.value}`
     );
 
     const data = await response.json();
@@ -25,7 +24,7 @@ async function performSearch() {
     if (data.Response == "True" && Array.isArray(data.Search)) {
       const fetchPromises = data.Search.map(async (id) => {
         const response = await fetch(
-          `https://www.omdbapi.com/?apikey=${apiKey}&i=${id.imdbID}`
+          `https://www.omdbapi.com/?apikey=f3f350ca&i=${id.imdbID}`
         );
         const data = await response.json();
         return data;
